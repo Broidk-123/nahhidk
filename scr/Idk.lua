8e6a3b1c0f81256c14b5ebea57372680955c66f2
Broidk-123 <sapianjared1@gmail.com>
2026-06-03 06:45:08 +0800

    Update Idk.lua

--- scr/Idk.lua
+++ scr/Idk.lua
@@ -249,10 +249,10 @@ local Window = WindUI:CreateWindow({
             print("clicked")
         end,
     },
-		Topbar = {
+		--[[Topbar = {
 			Height = 46,
 			Type = "Default", -- "Default" or "Mac"
-		},
+		},]]
 		
 })
 
